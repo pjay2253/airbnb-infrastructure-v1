b0d844ca204c62c0203ba3fae71c2090eae657ca
pjay2253 <101237111+pjay2253@users.noreply.github.com>
2022-12-02 17:09:35 -0500

    fix for best code base scan practice

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "AMI_ID" {
-    type = string
-    description = "AMI ID"
-    default = "ami-0149b2da6ceec4bb0"
+  type        = string
+  description = "AMI ID"
+  default     = "ami-0149b2da6ceec4bb0"
 }
 
 variable "INSTANCE_TYPE" {
-    type = string
-    description = "type of EC2 instance"
-    default = "t3.micro"
+  type        = string
+  description = "type of EC2 instance"
+  default     = "t3.micro"
 }
